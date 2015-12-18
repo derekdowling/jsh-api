73158d20a5e789218b7ca059094478462a2a090e
Derek Dowling <ddowling@justin.tv>
2015-12-17 17:32:56 -0800

    Logging support for intentional ISE

--- logger.go
+++ logger.go
@@ -24,10 +24,16 @@ var Logger ILogger = log.New(os.Stderr, "jshapi: ", log.LstdFlags)
 // SendAndLog is a jsh wrapper function that first prepares a jsh.Sendable response,
 // and then handles logging 5XX errors that it encounters in the process.
 func SendAndLog(ctx context.Context, w http.ResponseWriter, r *http.Request, sendable jsh.Sendable) {
+
+	intentionalErr, isType := sendable.(*jsh.Error)
+	if isType {
+		Logger.Printf("Returning ISE for: %s", intentionalErr.Internal())
+	}
+
 	response, err := sendable.Prepare(r, true)
 
 	if err != nil && response.HTTPStatus >= 500 {
-		Logger.Printf("Error: %s\n", err.Internal())
+		Logger.Printf("Error preparing response: %s\n", err.Internal())
 	}
 
 	sendErr := jsh.SendResponse(w, r, response)

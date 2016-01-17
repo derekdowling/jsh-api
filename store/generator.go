38c294ec329af497c2a13acf4aefdb7f0695ee51
Derek Dowling <ddowling@justin.tv>
2016-01-17 13:21:15 -0800

    Adding basic template logic for scaffolding a CRUD store

---
+++ store/generator.go
@@ -0,0 +1,72 @@
+package store
+
+// generator implements a gen(https://github.com/clipperhouse/gen) TypeWriter for
+// stubbing a store.CRUD implementation and corresponding tests.
+
+import (
+	"io"
+	"log"
+
+	"github.com/clipperhouse/typewriter"
+)
+
+func init() {
+	err := typewriter.Register(NewStoreWriter())
+	if err != nil {
+		panic(err)
+	}
+}
+
+// StoreWriter implements gen/TypeWriter
+type StoreWriter struct{}
+
+// NewStoreWriter is a simple constructor
+func NewStoreWriter() *StoreWriter {
+	return &StoreWriter{}
+}
+
+// Name sets the gen tag for the StoreWriter
+func (sw *StoreWriter) Name() string {
+	return "store"
+}
+
+// Imports represent the dependencies of the generated code.
+func (sw *StoreWriter) Imports(t typewriter.Type) (result []typewriter.ImportSpec) {
+
+	imports := []typewriter.ImportSpec{
+		typewriter.ImportSpec{
+			Path: "github.com/derekdowling/go-json-spec-handler",
+		},
+		typewriter.ImportSpec{
+			Path: "github.com/derekdowling/jsh-api/store",
+		},
+	}
+
+	// none
+	return result
+}
+
+func (sw *StoreWriter) Write(w io.Writer, t typewriter.Type) error {
+	tag, found := t.FindTag(sw)
+	if !found {
+		log.Printf("Store type '%s' not found\n", t.Name)
+		return nil
+	}
+
+	license := `// This file implement store.CRUD https://github.com/derekdowling/jsh-api/store`
+	if _, err := w.Write([]byte(license)); err != nil {
+		return err
+	}
+
+	tmpl, err := templates.ByTag(t, tag)
+
+	if err != nil {
+		return err
+	}
+
+	if err := tmpl.Execute(w, t); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ store/template.go
@@ -0,0 +1,97 @@
+package set
+
+import "github.com/clipperhouse/typewriter"
+
+var templates = typewriter.TemplateSlice{
+	store,
+}
+
+var store = &typewriter.Template{
+	Name: "Store",
+	Text: `
+// {{.Name}}Store holds state for {{.Name}} Storage actions
+type {{.Name}}Store struct{}
+
+// New{{.Name}}Set creates and returns a reference to an empty set.
+func New{{.Name}}() {{.Pointer}}{{.Name}}Store {
+    return &{{.Name}}Store{}
+}
+
+// ToSlice returns the elements of the current set as a slice
+func (set {{.Name}}Set) ToSlice() []{{.Pointer}}{{.Name}} {
+    var s []{{.Pointer}}{{.Name}}
+    for v := range set {
+        s = append(s, v)
+    }
+    return s
+}
+
+// Save handles POST /{{.Name}}
+func (store {{.Pointer}}{{.Name}}Store) Save(ctx context.Context, object *jsh.Object) (*jsh.Object, jsh.ErrorType) {
+    {{.Name}} := &{{.Name}}{}
+    unmarshalErr := object.Unmarshal("{{.Name}}", {{.Name}})
+    if err != nil {
+	return nil, unmarshalErr
+    }
+
+    // TODO: Save {{.Name}}
+
+    marshalErr := object.Marshal({{.Name}})
+    if marshalErr != nil {
+	return nil, marshalErr
+    }
+
+    return object, nil
+}
+
+// Save handles GET /{{.Name}}/:id
+func (store {{.Pointer}}{{.Name}}Store) Get(ctx context.Context, id string) (*jsh.Object, jsh.ErrorType) {
+    var {{.Name}} *{{.Name}}
+
+    // TODO: Get {{.Name}}
+
+    object, err := jsh.NewObject(id, "{{.Name}}", {{.Name}})
+    if err != nil {
+	return nil, err
+    }
+
+    return object, nil
+}
+
+// Save handles GET /{{.Name}}
+func (store {{.Pointer}}{{.Name}}Store) List(ctx context.Context) (jsh.List, jsh.ErrorType) {
+    list := &jsh.List{}
+
+    // TODO: Get {{.Name}} list
+
+    return list, nil
+}
+
+// Save handles PATCH /{{.Name}}/:id
+func (store {{.Pointer}}{{.Name}}Store) Update(ctx context.Context, object *jsh.Object) (*jsh.Object, jsh.ErrorType) {
+    {{.Name}} := &{{.Name}}{}
+    unmarshalErr := object.Unmarshal("{{.Name}}", {{.Name}})
+    if unmarshalErr != nil {
+	return nil, unmarshalErr
+    }
+
+    id := object.ID
+
+    // TODO: Update {{.Name}} object
+
+    marshalErr := object.Marshal({{.Name}})
+    if marshalErr != nil {
+	return nil, marshalErr
+    }
+
+    return object, nil
+}
+
+// Save handles DELETE /{{.Name}}/:id
+func (store {{.Pointer}}{{.Name}}Store) Delete(ctx context.Context, id string) jsh.ErrorType {
+    // TODO: implement Delete logic
+    return nil
+}
+`,
+	TypeConstraint: typewriter.Constraint{},
+}
